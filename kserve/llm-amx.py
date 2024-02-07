d45faa94b502788734f9a3dae6be33e32b76567c
Le Yao <le.yao@intel.com>
2024-02-07 17:53:59 +0800

    amx enabled
    Signed-off-by: Le Yao <le.yao@intel.com>

---
+++ kserve/llm-amx.py
@@ -0,0 +1,43 @@
+import torch
+import intel_extension_for_pytorch as ipex
+import json
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from kserve import Model, ModelServer
+
+from ray import serve
+
+@serve.deployment(
+    name="llmserving", 
+    ray_actor_options={"num_cpus": 3},
+    num_replicas=1,
+)
+class LLaMAModel(Model):
+    def __init__(self):
+        self.name = "llama-model"
+        super().__init__(self.name)
+        self.model_dir = "./llama-2-7b-chat-hf"
+        self.model = None
+        self.tokenizer = None
+        self.load()
+
+    def load(self):
+        # Load the tokenizer and model here
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_dir)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_dir)
+        self.model.eval()
+        self.model = self.model.to(memory_format=torch.channels_last)
+        self.model = ipex.llm.optimize(self.model.eval(), dtype=torch.bfloat16)
+
+    def predict(self, request, headers):
+        # Check if request needs to be decoded from bytes
+        data = json.loads(request.decode('utf-8'))
+
+        input_text = data["instances"][0]["text"]
+        input_ids = self.tokenizer.encode(input_text, return_tensors='pt')
+        with torch.no_grad(), torch.inference_mode(), torch.cpu.amp.autocast():
+            output = self.model.generate(input_ids, max_length=50)
+        response_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        return {"predictions": [response_text]}
+
+if __name__ == "__main__":
+    ModelServer().start({"llmserving": LLaMAModel})
